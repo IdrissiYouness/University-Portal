b4bb5203de8944393c6eeb5d31f355bdcf82b0d0
Youness Sghiouri Idrissi <zahirkamarul@outlook.com>
2025-05-18 10:53:53 +0100

    fix: remove unecessary js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,1 @@
-function handleFileChange() {
-    const fileInput = document.getElementById('real-file');
-    if (fileInput.files.length > 0) {
-      alert(`Selected file: ${fileInput.files[0].name}`);
-    }
-}
+//js scripts goes here
